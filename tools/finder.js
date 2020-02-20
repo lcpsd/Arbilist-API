0bce6240126c2111a0cb421781827ba081bc3744
lucas <lucascardosopsd.@gmail.com>
2020-02-20 03:53:08 -0300

    array of symbols creator concluded

--- tools/finder.js
+++ tools/finder.js
@@ -2,12 +2,23 @@ const classMaker = require('./classMaker')
 
 async function init(symbol, btcQty, exchangeNames){
     
+    let symbolsObject = new Array
     for(let exchangeName of exchangeNames){
+
+        let symbolObject = new Object
+
         let exchange = classMaker(exchangeName)
 
         let lastPrice = await exchange.currentPrice(symbol)
 
+        symbolObject["symbol"] = symbol
+        symbolObject["currentPrice"] = lastPrice
+        symbolObject["exchange"] = exchangeName
+
+        symbolsObject.push(symbolObject)
     }
+
+    console.log(symbolsObject)
 
 }
 

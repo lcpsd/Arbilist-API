7c078a107b7aef03697d8fd71054d7fe330717fa
lucas cardoso <lucascardosopsd@gmail.com>
2020-03-11 01:48:37 -0300

    adminAuth / userAuth fixed

--- src/middlewares/adminAuth.js
+++ src/middlewares/adminAuth.js
@@ -1,5 +1,16 @@
+let adminModel = require('../model/admin')
+
 async function init (req, res, next){
-    if(req.session.email != undefined){
+
+    let adminObjects = await adminModel.findAll({raw: true})
+
+    let allAdminEmails = new Array
+
+    for(let obj of adminObjects){
+        allAdminEmails.push(obj.email)
+    }
+
+    if(req.session.email != undefined && allAdminEmails.includes(req.session.email)){
         return next()
     }
     return res.json({err: "unautorized"})
--- src/middlewares/userAuth.js
+++ src/middlewares/userAuth.js
@@ -1,16 +1,6 @@
-let adminModel = require('../model/admin')
-
 async function init (req, res, next){
 
-    let adminObjects = await adminModel.findAll({raw: true})
-
-    let allAdminEmails = new Array
-
-    for(let obj of adminObjects){
-        allAdminEmails.push(obj.email)
-    }
-
-    if(req.session.email != undefined && allAdminEmails.includes(req.session.email)){
+    if(req.session.email != undefined ){
         return next()
     }
     return res.json({err: "unautorized"})

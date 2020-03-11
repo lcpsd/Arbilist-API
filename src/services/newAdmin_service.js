8bf3184b1fee9a9d31a07e5745caa915ab531c86
lucas cardoso <lucascardosopsd@gmail.com>
2020-03-11 01:43:38 -0300

    create admin by root password implemented

--- src/services/newAdmin_service.js
+++ src/services/newAdmin_service.js
@@ -4,12 +4,18 @@ async function newAdmin(req, res, adminModel){
 
     let {email} = req.body
     let {passwd} = req.body
+    let {rootPasswd} = req.body
     let salt = await bcrypt.genSalt(10)
     let hash = await bcrypt.hash(passwd, salt)
 
+    let rootUser = await adminModel.findOne({raw: true, where: {email: "root@root.com"}})
     let find = await adminModel.findOne({raw:true, where: {email: email} })
 
+    let rootValidator = bcrypt.compare(rootPasswd, rootUser.passwd)
+
     if(find) return {body: "admin_aready_exists"}
+
+    if(!rootValidator) return {body: "incorrect_root_password"}
 
     let adminCreation = await adminModel.create({
         email: email,

7a38bf9c18417e89db04429509aa227585972cba
lucas cardoso <lucascardosopsd@gmail.com>
2020-05-22 13:22:57 -0300

    seeders fixed. field name to user model

--- src/db/migrations/20200311001227-create-user.js
+++ src/db/migrations/20200311001227-create-user.js
@@ -8,11 +8,17 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      name:{
+        type: Sequelize.STRING,
+        allowNull: false
+      },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       passwd: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       createdAt: {
         allowNull: false,
